ba436db5d4c398c09f6a496b0fc8ef3b1794b097
Artem <111070378+artomrevus@users.noreply.github.com>
2022-09-22 18:00:44 +0300

    Update lab01_2.c

--- OP/lab01/lab01_2.c
+++ OP/lab01/lab01_2.c
@@ -1,5 +1,5 @@
 /*
- За заданим натуральним восьмизначним числом знаходить подвоєний добуток ненульовиx цифр цього числа та кількість нулів. 
+ За заданим натуральним восьмизначним числом знаходить подвоєний добуток ненульовиx цифр цього числа та кількість нулів.
  */
  
 #include <stdio.h>
@@ -13,64 +13,70 @@ int main() {
     printf("Введіть восьмизначне число: ");
     scanf("%d", &num);
     
-    x8 = num % 10;
-    if(x8 == 0)
-        zero ++ ;
-    else
-        result *= x8;
-
     
-    x7 = (int)(num % 100) / 10;
-    if(x7 == 0)
-        zero ++ ;
-    else
-        result *= x7;
-    
-    
-    x6 = (int)(num % 1000) / 100;
-    if(x6 == 0)
-        zero ++ ;
-    else
-        result *= x6;
-    
-    
-    x5 = (int)(num % 10000) / 1000;
-    if(x5 == 0)
-        zero ++ ;
-    else
-        result *= x5;
-    
-    
-    x4 = (int)(num % 100000) / 10000;
-    if(x4 == 0)
-        zero ++ ;
-    else
-        result *= x4;
-    
-    
-    x3 = (int)(num % 1000000) / 100000;
-    if(x3 == 0)
-        zero ++ ;
-    else
-        result *= x3;
-    
-    
-    x2 = (int)(num % 10000000) / 1000000;
-    if(x2 == 0)
-        zero ++ ;
-    else
-        result *= x2;
-    
-    
-    x1 = (int) num / 10000000;
-    if(x1 == 0)
-        zero ++ ;
-    else
-        result *= x1;
-    
-
-    printf("Кількість нулів у введеному числі: %hd\n", zero);
-    printf("Подвоєний добуток ненульових цифр введеного числа = %d\n", result);
+    if(num >= 10000000 && num <= 99999999) {
+        
+        x8 = num % 10;
+        if(x8 == 0)
+            zero ++ ;
+        else
+            result *= x8;
+        
+        
+        x7 = (int)(num % 100) / 10;
+        if(x7 == 0)
+            zero ++ ;
+        else
+            result *= x7;
+        
+        
+        x6 = (int)(num % 1000) / 100;
+        if(x6 == 0)
+            zero ++ ;
+        else
+            result *= x6;
+        
+        
+        x5 = (int)(num % 10000) / 1000;
+        if(x5 == 0)
+            zero ++ ;
+        else
+            result *= x5;
+        
+        
+        x4 = (int)(num % 100000) / 10000;
+        if(x4 == 0)
+            zero ++ ;
+        else
+            result *= x4;
+        
+        
+        x3 = (int)(num % 1000000) / 100000;
+        if(x3 == 0)
+            zero ++ ;
+        else
+            result *= x3;
+        
+        
+        x2 = (int)(num % 10000000) / 1000000;
+        if(x2 == 0)
+            zero ++ ;
+        else
+            result *= x2;
+        
+        
+        x1 = (int) num / 10000000;
+        if(x1 == 0)
+            zero ++ ;
+        else
+            result *= x1;
+        
+        printf("Кількість нулів у введеному числі: %hd\n", zero);
+        printf("Подвоєний добуток ненульових цифр введеного числа = %d\n", result);
+        
+    } else
+        printf("Ви ввели не восьмизначне число!\n");
     
     return 0;
 }
+
