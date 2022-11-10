3ae456e17b61bf327196856d1646206c50177a4c
Artem <111070378+artomrevus@users.noreply.github.com>
2022-11-10 16:23:01 +0200

    Create table_function.h

---
+++ OP/lab05/table_function.h
@@ -0,0 +1,9 @@
+#ifndef TABLE_FUNCTION_H
+#define TABLE_FUNCTION_H
+
+float calculation_1(float x);
+float calculation_2(float x, float * y);
+void tabulation_1(float a, float b, float h);
+void tabulation_2(float a, float b, float h);
+
+#endif
