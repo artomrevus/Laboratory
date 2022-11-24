855eab1b05ec9ad0941bab63008abecfd7932bd3
Artem <111070378+artomrevus@users.noreply.github.com>
2022-11-24 23:37:10 +0200

    Create func_shift.h

---
+++ OP/lab06/lab06_1/func_shift.h
@@ -0,0 +1,11 @@
+#ifndef func_shift_h
+#define func_shift_h
+#define n 10
+
+void scan_arr(int [n][n], int, int);
+void print_arr(int [n][n], int, int);
+int maxx(int [n][n], int, int, int);
+void teleport(int [n][n], int, int, int);
+
+
+#endif
