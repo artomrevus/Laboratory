cfbdb517b767ccf52e3cf787a9c7faad7cf1d4b6
Artem <111070378+artomrevus@users.noreply.github.com>
2022-12-01 20:51:39 +0200

    Create sort.h

---
+++ OP/lab07/lab07_1/sort.h
@@ -0,0 +1,11 @@
+#ifndef sort_h
+#define sort_h
+
+void swap(int array[], long pos1, long pos2);
+void qs_sort(int array[], long start, long end);
+void bubble_sort(int array[], int size);
+void arrange_check(int array[], int size);
+void array_equal(int array_1[], int * array_2, int size);
+void print_mass(int arr[]);
+
+#endif 
