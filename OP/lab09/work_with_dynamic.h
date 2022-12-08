bc1c7fd54019ee99c2200c8a8110add7de9a60e8
Artem <111070378+artomrevus@users.noreply.github.com>
2022-12-08 16:13:57 +0200

    Create work_with_dynamic.h

---
+++ OP/lab09/work_with_dynamic.h
@@ -0,0 +1,10 @@
+#ifndef work_with_dynamic_h
+#define work_with_dynamic_h
+
+#include <stdio.h>
+
+void scan_elem(int ** pMatr, int n, int m);
+void print_second_column(int ** pMatr, int n, int m);
+void clean_memory(int ** pMatr, int n, int m);
+
+#endif
