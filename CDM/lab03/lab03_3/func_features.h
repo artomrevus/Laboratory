7a17e11ab960b5bdd95f909e4e1619c21ce06726
Artem <111070378+artomrevus@users.noreply.github.com>
2022-12-08 20:40:46 +0200

    Create func_features.h

---
+++ CDM/lab03/lab03_3/func_features.h
@@ -0,0 +1,10 @@
+#ifndef func_features_h
+#define func_features_h
+#define N 4
+
+int reflexive(int arr[N][N]);
+int symetric(int arr[N][N]);
+int transistive(int arr[N][N]);
+void relation(int arr[N][N]);
+
+#endif
